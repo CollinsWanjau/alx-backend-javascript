32af167146483cfc3dc6c9296b73868eb5a493d9
colloso999 <tripperskripper@gmail.com>
2023-09-27 17:32:23 +0300

    Appended three handlers to the function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 export default function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'Success' };
+
   return promise
-    .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error())
+    .then(() => body)
+    .catch(() => new Error())
     .finally(() => { console.log('Got a response from the API'); });
 }
