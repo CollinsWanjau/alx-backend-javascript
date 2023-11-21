4e55d9ea6c5feabea4ac6979bd27207ec68b1cf9
colloso999 <tripperskripper@gmail.com>
2023-11-21 09:01:05 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,12 +1,3 @@
-#!/usr/bin/env node
-// Create a function named countStudents. It should accept a path in argument
-// The script should attempt to read the database file synchronously
-// If the database is not available, it should throw an error with the text Cannot load the database
-// eslint-disable-next-line max-len
-// If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
-// eslint-disable-next-line max-len
-// It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
-// CSV file can contain empty lines (at the end) - and they are not a valid student!
 const fs = require('fs');
 
 function countStudents(path) {
