5d74664b4738a94d368a6f0f4a47030b29bdac89
colloso999 <tripperskripper@gmail.com>
2023-11-21 13:17:29 +0300

    Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,75 @@
+// It should be assigned to the variable app and this one must be exported
+// HTTP server should listen on port 1245
+// It should return plain text
+// When the URL path is /, it should display Hello Holberton School! in the page body
+// When the URL path is /students, it should display This is the list of our students followed by the same content as the file 3-read_file_async.js (with and without the database) - the name of the database must be passed as argument of the file
+// CSV file can contain empty lines (at the end) - and they are not a valid student!
+const http = require('http');
+const { readFile } = require('fs');
+const hostname = '127.0.0.1';
+const port = 12345;
+
+function countStudents (path) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        let output = `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(',')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  // eslint-disable-next-line default-case
+  switch (req.url) {
+    case '/':
+      res.write('Hello Holberton School!');
+      res.end();
+      break;
+    case '/students':
+      res.write('This is the list of our students\n');
+      countStudents(process.argv[2].toString()).then((output) => {
+        const slicedOutput = output.slice(0, -1);
+        res.end(slicedOutput);
+      }).catch(() => {
+        res.statusCode = 404;
+        res.end('Cannot load the database');
+      });
+  }
+});
+
+app.listen(port, hostname, () => {});
+
+module.exports = app;
