b996a90b9d6a6fd8f119a164da9eaea312c9e3c1
colloso999 <tripperskripper@gmail.com>
2023-11-23 09:54:44 +0300

    Spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi (totalAmount, totalShipping) {
-  const a = totalAmount;
-  const b = totalShipping;
+  let a = totalAmount;
+  let b = totalShipping;
   const utilsSum = Utils.calculateNumber('SUM', a, b);
   console.log(`The total is: ${utilsSum}`);
 }
