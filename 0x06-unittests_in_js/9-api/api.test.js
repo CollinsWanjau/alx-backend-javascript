c2ebf3c072eeed7be2876c5d90e1ba954eeb0d32
colloso999 <tripperskripper@gmail.com>
2023-11-23 19:53:26 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -16,6 +16,7 @@ describe('apitest', () => {
       });
     });
   });
+
   describe('GET /cart/12', () => {
     it('should listen to the correct status code', (done) => {
       const options = {
@@ -29,4 +30,45 @@ describe('apitest', () => {
       });
     });
   });
+
+  describe('GET /cart/123', () => {
+    it('should listen to the correct status code', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/123',
+        method: 'GET'
+      };
+      request(options, (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 123');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/a23', () => {
+    it('should listen to the correct status code', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/a23',
+        method: 'GET'
+      };
+      request(options, (err, res, body) => {
+        expect(res.statusCode).to.equal(404); 
+        done();
+      });
+    });
+  });
+  describe('GET /cart/23bc', () => {
+    it('should listen to the correct status code', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/23bc',
+        method: 'GET'
+      };
+      request(options, (err, res, body) => {
+        expect(res.statusCode).to.equal(404); 
+        done();
+      });
+    });
+  });
+
 });
+
